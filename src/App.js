ae22b51df38682b8f7aa82d46f8030f2f9092c4d
CihanLoyan <cihanloyan@gmail.com>
2022-01-18 19:54:29 +0300

    App.js file edited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryList from './CategoryList';
+import Navbar from './Navbar';
+import ProductList from './ProductList';
+import { Container, Row, Col } from 'reactstrap';
+import { Component } from 'react/cjs/react.production.min';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = { currentCategory: "", products: [] }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.name })
+    this.getProducts(category.categoryID)
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = (categoryID) => {
+
+    let url = 'http://localhost:3000/products'
+    if(categoryID) {
+      url += "?categoryID=" + categoryID
+    }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ products: data }))
+  }
+
+  render() {
+    let productInfo = { title: "Product List" }
+    let categoryInfo = { title: "Category List" }
+    return (
+      <div>
+        <Container>
+          <Navbar />
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+
+            <Col xs="9">
+              <ProductList products={this.state.products} currentCategory={this.state.currentCategory} info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+
+      </div>
+    );
+  }
 }
-
-export default App;
