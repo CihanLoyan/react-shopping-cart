ec94fa07953e001aa7222de0a435c26d1edf9743
CihanLoyan <cihanloyan@gmail.com>
2022-01-19 05:13:16 +0300

    changeCategory getProducts, addToCart, removeFromCart functions created and link used with route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import CategoryList from './CategoryList';
-import Navbar from './Navbar';
+import Navi from './Navi';
 import ProductList from './ProductList';
 import { Container, Row, Col } from 'reactstrap';
 import { Component } from 'react/cjs/react.production.min';
 import './App.css'
+import alertify from 'alertifyjs';
+import { Switch, Router, Route } from 'react-router-dom'
+import NotFound from './NotFound'
+import CartList from './CartList'
 
 export default class App extends Component {
 
-  state = { currentCategory: "", products: [] }
+  state = { currentCategory: "", products: [], cart: [] }
 
   changeCategory = (category) => {
     this.setState({ currentCategory: category.name })
@@ -21,12 +25,30 @@ export default class App extends Component {
   getProducts = (categoryID) => {
 
     let url = 'http://localhost:3000/products'
-    if(categoryID) {
+    if (categoryID) {
       url += "?categoryID=" + categoryID
     }
     fetch(url)
       .then(response => response.json())
       .then(data => this.setState({ products: data }))
+  }
+
+  addToCart = (product) => {
+    let newCart = this.state.cart
+    var addedItem = newCart.find(c => c.product.productID === product.productID)
+    if (addedItem) {
+      addedItem.quantity += 1
+    } else {
+      newCart.push({ product: product, quantity: 1 })
+    }
+    this.setState({ cart: newCart })
+    alertify.success(`${product.name} added to cart `, 3)
+  }
+
+  removeFromCart = (product) => {
+    let newCart = this.state.cart.filter(c => c.product.productID !== product.productID)
+    this.setState({ cart: newCart })
+    alertify.error(`${product.name} removed from cart`, 3)
   }
 
   render() {
@@ -35,14 +57,32 @@ export default class App extends Component {
     return (
       <div>
         <Container>
-          <Navbar />
+          <Navi cart={this.state.cart} removeFromCart={this.removeFromCart} />
           <Row>
             <Col xs="3">
               <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
             </Col>
 
             <Col xs="9">
-              <ProductList products={this.state.products} currentCategory={this.state.currentCategory} info={productInfo} />
+              <Switch>
+                <Route exact path="/" render={props => (
+                  <ProductList
+                    {...props}
+                    products={this.state.products}
+                    addToCart={this.addToCart}
+                    currentCategory={this.state.currentCategory}
+                    info={productInfo} />
+                )}
+                />
+                <Route exact path="/cart" render={props => (
+                  <CartList
+                    {...props}
+                    cart={this.state.cart}
+                    removeFromCart={this.removeFromCart}
+                  />
+                )} 
+                />
+              </Switch>
             </Col>
           </Row>
         </Container>
