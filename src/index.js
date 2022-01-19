d9aaa0584cead498c797bfee9dd7e6d791e3c6b5
CihanLoyan <cihanloyan@gmail.com>
2022-01-19 05:06:59 +0300

    react-router-dom and alertifyjs imported to index.js file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'alertifyjs/build/css/alertify.min.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
